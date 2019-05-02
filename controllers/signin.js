eb10bd15ad1b3e263e8876e87b47fff15eac5f3c
johndev86 <johnlee@Johns-MacBook-Pro.local>
2019-05-02 15:13:17 -0400

    heroku debug log

--- controllers/signin.js
+++ controllers/signin.js
@@ -13,6 +13,7 @@ const handleSignin = (req, res, db, bcrypt) => {
 
     return db.select('email', 'hash').from('login').where('email','=',email)
     .then(login=>{
+        console.log(login);
         if (login.length && bcrypt.compareSync(password,login[0].hash)) {
             return db.select('*').from('users').where('email','=',email)
             .then(user => user[0])
